9401c7514e963fc23ba7550d603703e74f2b436b
fluke10006 <fluke10006@hotmail.co.th>
2019-11-23 17:02:41 +0700

    first commit

---
+++ src/th/co/cdgs/application/WorkshopApplication.java
@@ -0,0 +1,9 @@
+package th.co.cdgs.application;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/api")
+public class WorkshopApplication extends Application{
+
+}
---
+++ src/th/co/cdgs/bean/CustomerDto.java
@@ -0,0 +1,54 @@
+package th.co.cdgs.bean;
+
+public class CustomerDto {
+	private Long customerId;
+	private String FirstName;
+	private String LastName;
+	private String fullName;
+	private String address;
+	private String tel;
+	public Long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+	public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String FirstName) {
+		this.FirstName = FirstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String LastName) {
+		this.LastName = LastName;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	private String email;
+
+}
